c249ac3b8482e70aaa7eb4e7e06a81555bf4b40a
Katrin Andraschko <katrin.andraschko@dla-marbach.de>
2025-11-11 16:26:59 +0100

    Test für Bestandsübersicht anpassen (#5803)

--- tests/bestandsübersicht.spec.ts
+++ tests/bestandsübersicht.spec.ts
@@ -1,15 +1,28 @@
-// @ts-check
 import { test, expect } from '@playwright/test';
 
-test('kafka', async ({ page }) => {
-  await page.goto('bestandsuebersicht');
-  await page.getByRole('textbox', { name: 'Suche in der Bestandsübersicht' }).click();
-  await page.getByRole('textbox', { name: 'Suche in der Bestandsübersicht' }).fill('kafka');
-  // Öffne die erste Ebene des Baums (Buchstabe)
-  await page.getByRole('article').filter({ hasText: 'K' }).getByRole('link').click()
-  // Öffne die zweite Ebene des Baums (Personendatensatz)
-  await page.locator('#PE00001005').getByRole('link', { name: '' }).click();
-  // Erwarte bestimmten Bestandstitel
-  await page.waitForLoadState();
-  await expect(page.locator('#collectiontree')).toContainText('Sammlung, Handschriftensammlung [A:Kafka, Franz]');
+test('bestandsübersicht', async ({ page }) => {
+
+  // Man gelangt zur Bestandsübersicht über den Einstieg unterhalb des Suchschlitzes
+  await page.goto('katalog');
+  await page.getByRole('link', { name: 'Bestandsübersicht' }).click();
+  await expect(page.getByRole('banner')).toContainText('Bestandsübersicht');
+  // Man gelangt zur Systematik über einen Teaser
+  await page.goto('katalog');
+  await page.getByRole('heading', { name: 'Nachlässe und Sammlungen' }).click();
+  await expect(page.getByRole('banner')).toContainText('Bestandsübersicht');
+
+  // Eingabe Begriff Suchschlitz, bewirkt Aufklappen der ersten Hierarchie-Ebene,
+  // von dort gelangt man auf die tieferen Hierarchie-Ebenen bis der Suchbegriff angezeigt wird
+  await page.getByRole('textbox', { name: 'Suche in der Bestandsübersicht' }).pressSequentially('kafka');
+  await page.waitForTimeout(1000);
+  await page.locator('#PE00001005').locator('span.icon.bel-pfeil-u01').click();
+  await page.locator('#BF00013240').locator('span.icon.bel-pfeil-u01').click();
+  await expect(page.locator('#BF00013127')).toContainText('Sammlung, Handschriften [D:Kafka, Franz°Weltsch]');
+
+  // mit dem Lupensymbol wird die Suche nach Treffern ausgelöst
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('#BF00013127').locator('span.icon.bel-lupe').click();
+  const page1 = await page1Promise;
+  await expect(page1.locator('.ctg-result-list').nth(1)).toContainText('D:Kafka, Franz°Weltsch');
+
 });
