0d52548000a8ddffd6ba1d6cba2652fafda840f0
Katrin Andraschko <katrin.andraschko@dla-marbach.de>
2025-11-13 09:58:12 +0100

    Timeout erhöhen

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
+  timeout: 45_000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
