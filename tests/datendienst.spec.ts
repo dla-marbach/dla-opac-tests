739314bd83ff0c17b5e7a3d101c39d44bbc06653
Katrin Andraschko <katrin.andraschko@dla-marbach.de>
2026-02-13 08:52:16 +0100

    Test an umstrukturierten Datendienst anpassen (#9146)

--- tests/datendienst.spec.ts
+++ tests/datendienst.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('fields', async ({ page }) => {
   await page.goto('https://dataservice.dla-marbach.de/');
   // Öffne Endpunkt /records
-  await page.getByRole('link', { name: '/records', exact: true }).click();
+  await page.getByRole('button', { name: 'GET /records', exact: true }).click({ force: true });
   // Erwarte einen bestimmten Feldnamen in der Tabelle
   await page.waitForLoadState();
   await expect(page.getByRole('table')).toContainText('filterDateRange_mv');
