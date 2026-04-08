daf58027b0f256d2f97d55da1579dd4fab780ed0
Felix Lohmeier <mail@felixlohmeier.de>
2026-04-08 07:46:08 +0000

    Test Detailseite: Absolute URL in relativen Pfad geändert

--- tests/detailseite.spec.ts
+++ tests/detailseite.spec.ts
@@ -299,7 +299,7 @@ test('normdaten', async ({ page }) => {
     expect(await page.locator('#content-area').locator('a:text("Bestände")').count()).toBe(1);
 
     // Deskriptor - Sachschlagwort
-    await page.goto('https://www.dla-marbach.de/find/opac/id/TH00038793/?tx_find_find%5Baction%5D=detail&tx_find_find%5Bcontroller%5D=Search&tx_find_find%5BqParam%5D=1');
+    await page.goto('find/opac/id/TH00038793');
     await expect(page.locator('#content-area')).toContainText('Deskriptor. - Sachschlagwort');
     await page.getByText('Weitere Details').click();
     await expect(page.locator('#content-area')).toContainText('Sachschlagwort');
